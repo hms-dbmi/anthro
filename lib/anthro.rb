ee863fb55b0e0094e17989f02541382354a8a66a
Joon Lee <seouri@gmail.com>
2025-03-20 19:52:06 +0000

    Initial commit

---
+++ lib/anthro.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anthro/version"
+
+module Anthro
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/anthro/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Anthro
+  VERSION = "0.1.0"
+end
